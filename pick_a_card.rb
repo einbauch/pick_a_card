85f3fe4557e317b931607361ddc48986da66ef5f
Alex Kuzmin <alex.kuzminx@gmail.com>
2016-04-21 01:21:25 +0300

    change card symbols to card unicode codes

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['♦', '♠', '♥', '♣']
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 
 puts values.sample + suits.sample
